dbb3e9acb929e6d5d9012193f2cc04e193232efb
Michael Devery <michaeldevery@gmail.com>
2015-05-16 22:26:08 +0200

    add repository trait

---
+++ src/Mickadoo/Yarnyard/Library/EntityRepository/RepositoryTrait.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Mickadoo\Yarnyard\Library\EntityRepository;
+
+use Mickadoo\Yarnyard\Bundle\UserBundle\Entity\UserRepository;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+trait RepositoryTrait
+{
+
+    /**
+     * @var UserRepository
+     */
+    protected  $userRepository;
+
+    /**
+     * @return UserRepository
+     */
+    public function getUserRepository()
+    {
+        if (! isset($this->userRepository)) {
+            $this->userRepository = $this->getContainer()->get('yarnyard.user.user.repository');
+        }
+
+        return $this->userRepository;
+    }
+
+    /**
+     * @return ContainerInterface
+     */
+    private function getContainer()
+    {
+        /** @noinspection PhpUndefinedFieldInspection */
+        return $this->container;
+    }
+
+}
