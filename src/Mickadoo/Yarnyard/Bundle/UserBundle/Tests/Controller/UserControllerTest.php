54fbd002b04f7a56621406fbb1e131f0fc11af1b
Michael Devery <michaeldevery@gmail.com>
2015-06-21 23:50:00 +0200

    get all users and patch user test working

--- src/Mickadoo/Yarnyard/Bundle/UserBundle/Tests/Controller/UserControllerTest.php
+++ src/Mickadoo/Yarnyard/Bundle/UserBundle/Tests/Controller/UserControllerTest.php
@@ -2,29 +2,48 @@
 
 namespace Mickadoo\Yarnyard\Bundle\UserBundle\Tests\Controller;
 
+use FOS\RestBundle\Util\Codes;
 use Mickadoo\Yarnyard\Library\Tests\ApiTestCase;
 
 class UserControllerTest extends ApiTestCase
 {
-    public function testSimple()
+    public function testGetAllUsers()
     {
-        $client = $this->createAuthorizedClient();
-
-        $user = $this->getUserWithToken();
-
-        $token = static::$kernel
-            ->getContainer()
-            ->get('doctrine')
-            ->getRepository('MickadooYarnyardAuthBundle:AccessToken')
-            ->findOneBy(['user' => $this->getUserWithToken()]);
-
-        $tokenString = 'Bearer ' . $token->getToken();
-        $authorizationHeaders = ['HTTP_AUTHORIZATION' => $tokenString];
-
-        $crawler = $client->request('GET', '/user', [], [], $authorizationHeaders);
-        $request = $client->getRequest();
+        $client = $this->getAuthorizedClient();
+        $client->request('GET', '/user');
         $response = $client->getResponse();
 
-        $this->assertTrue(1 === 1);
+        $this->assertEquals(Codes::HTTP_OK, $response->getStatusCode());
+
+        $responseContent = json_decode($response->getContent(), true);
+        $this->assertTrue(is_array($responseContent));
+
+        $usernames = [];
+        foreach ($responseContent as $user) {
+            $usernames[] = $user['username'];
+        }
+        $this->assertContains($this->getLoggedInUser()->getUsername(), $usernames);
+    }
+
+    public function testPatchUser()
+    {
+        $newUsername = 'philip';
+
+        $client = $this->getAuthorizedClient();
+        $client->request(
+            'PATCH',
+            '/user/' .$this->getLoggedInUser()->getId(),
+            [],
+            [],
+            [],
+            json_encode(['username'=>$newUsername])
+        );
+        $response = $client->getResponse();
+
+        $this->assertEquals(Codes::HTTP_OK, $response->getStatusCode());
+
+        $responseContent = json_decode($response->getContent(), true);
+        $this->assertEquals($newUsername, $responseContent['username']);
+        $this->assertEquals($this->getLoggedInUser()->getEmail(), $responseContent['email']);
     }
 }
