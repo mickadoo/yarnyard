a551ebb9b3032b11125cc85b204720cc3de3af01
mickadoo <michaeldevery@gmail.com>
2016-06-27 18:13:18 +0100

    add next participant fetcher

---
+++ src/YarnyardBundle/Service/NextParticipantFetcher.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace YarnyardBundle\Service;
+
+use YarnyardBundle\Entity\SentenceRepository;
+use YarnyardBundle\Entity\Story;
+use YarnyardBundle\Entity\User;
+use YarnyardBundle\Exception\YarnyardException;
+use YarnyardBundle\Util\DateInterval\IntervalCounter;
+
+class NextParticipantFetcher
+{
+    /**
+     * @var ParticipantSorter
+     */
+    protected $sorter;
+
+    /**
+     * @var SentenceRepository
+     */
+    protected $sentenceRepo;
+
+    /**
+     * @var IntervalCounter
+     */
+    protected $counter;
+
+    /**
+     * @param ParticipantSorter  $sorter
+     * @param SentenceRepository $sentenceRepo
+     * @param IntervalCounter    $counter
+     */
+    public function __construct(
+        ParticipantSorter $sorter,
+        SentenceRepository $sentenceRepo,
+        IntervalCounter $counter
+    ) {
+        $this->sorter = $sorter;
+        $this->sentenceRepo = $sentenceRepo;
+        $this->counter = $counter;
+    }
+
+    /**
+     * @param Story $story
+     *
+     * @return User
+     */
+    public function fetch(Story $story) : User
+    {
+        $participants = $this->sorter->getSortedParticipants($story);
+
+        if (empty($participants)) {
+            throw new YarnyardException('Can\'t get next participant cos there are none');
+        }
+
+        // default last contributor is the first in order
+        $lastContributorIndex = 0;
+        // timer for skips starts at story creation
+        $updatedAt = $story->getCreatedAt();
+
+        // if contributions exist find who did the last one
+        if ($this->sentenceRepo->hasSentences($story)) {
+            $lastSentence = $this->sentenceRepo->getMostRecent($story);
+            $updatedAt = $lastSentence->getCreatedAt();
+            $lastContributorIndex = array_search(
+                $lastSentence->getCreatedBy(),
+                $participants,
+                true
+            );
+        }
+
+        $now = new \DateTime();
+        $duration = $story->getSkipTurnDuration();
+        $numSkips = $this->counter->countBetween($updatedAt, $now, $duration);
+
+        // next = most recent contributor + num skips + 1
+        $next = $lastContributorIndex + $numSkips + 1;
+
+        // in case it's done a full round of skips and/or gone to the next round
+        $next = $next % count($participants);
+
+        return $participants[$next];
+    }
+}
