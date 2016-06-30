5e0025dcf2b8b443f460c7578f47263b13dcb413
mickadoo <michaeldevery@gmail.com>
2016-06-30 10:51:29 +0100

    update sentence created event listener

--- src/YarnyardBundle/EventListener/Sentence/SentenceCreatedEventListener.php
+++ src/YarnyardBundle/EventListener/Sentence/SentenceCreatedEventListener.php
@@ -3,20 +3,52 @@
 namespace YarnyardBundle\EventListener\Sentence;
 
 use YarnyardBundle\Event\Sentence\SentenceCreatedEvent;
+use YarnyardBundle\Service\RandomParticipantAdder;
+use YarnyardBundle\Service\StoryCompletionService;
 
 class SentenceCreatedEventListener
 {
+    /**
+     * @var StoryCompletionService
+     */
+    protected $completionService;
+
+    /**
+     * @var RandomParticipantAdder
+     */
+    protected $participantAdder;
+
+    /**
+     * @param StoryCompletionService $completionService
+     * @param RandomParticipantAdder $participantAdder
+     */
+    public function __construct(
+        StoryCompletionService $completionService,
+        RandomParticipantAdder $participantAdder
+    ) {
+        $this->completionService = $completionService;
+        $this->participantAdder = $participantAdder;
+    }
+
     /**
      * @param SentenceCreatedEvent $event
      */
     public function onSentenceCreated(SentenceCreatedEvent $event)
     {
         $sentence = $event->getSentence();
+        $story = $sentence->getStory();
 
-        // check if story should be completed
+        if ($this->completionService->shouldComplete($story)) {
+            $this->completionService->complete($story);
+
+            return;
+        }
 
         // if random and not completed add new participant
+        if ($this->participantAdder->shouldAddParticipant($story)) {
+            $this->participantAdder->addRandomParticipant($story);
+        }
 
-        // notify next participant
+        // todo notify next participant
     }
 }
