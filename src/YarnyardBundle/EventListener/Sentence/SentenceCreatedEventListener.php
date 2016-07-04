970327dbc37b7c28c377b7fe2e73deb4a7858210
mickadoo <michaeldevery@gmail.com>
2016-07-04 19:48:30 +0100

    update sentence created event listener to notify next participant

--- src/YarnyardBundle/EventListener/Sentence/SentenceCreatedEventListener.php
+++ src/YarnyardBundle/EventListener/Sentence/SentenceCreatedEventListener.php
@@ -2,9 +2,12 @@
 
 namespace YarnyardBundle\EventListener\Sentence;
 
+use YarnyardBundle\Entity\User;
 use YarnyardBundle\Event\Sentence\SentenceCreatedEvent;
 use YarnyardBundle\Service\RandomParticipantAdder;
 use YarnyardBundle\Service\StoryCompletionService;
+use YarnyardBundle\Service\TurnTracker;
+use YarnyardBundle\Util\RabbitMQ\Connection;
 
 class SentenceCreatedEventListener
 {
@@ -19,15 +22,31 @@ class SentenceCreatedEventListener
     protected $participantAdder;
 
     /**
+     * @var Connection
+     */
+    protected $rabbit;
+
+    /**
+     * @var TurnTracker
+     */
+    protected $turnTracker;
+
+    /**
      * @param StoryCompletionService $completionService
      * @param RandomParticipantAdder $participantAdder
+     * @param Connection             $rabbit
+     * @param TurnTracker            $turnTracker
      */
     public function __construct(
         StoryCompletionService $completionService,
-        RandomParticipantAdder $participantAdder
+        RandomParticipantAdder $participantAdder,
+        Connection $rabbit,
+        TurnTracker $turnTracker
     ) {
         $this->completionService = $completionService;
         $this->participantAdder = $participantAdder;
+        $this->rabbit = $rabbit;
+        $this->turnTracker = $turnTracker;
     }
 
     /**
@@ -49,6 +68,32 @@ class SentenceCreatedEventListener
             $this->participantAdder->addRandomParticipant($story);
         }
 
-        // todo notify next participant
+        // get current turn user
+        $currentTurnUser = $this->turnTracker->getCurrentTurnUser($story);
+
+        // in stories with only one user we don't want to notify them again
+        if ($sentence->getCreatedBy() === $currentTurnUser) {
+            return;
+        }
+
+        $this->notifyNextParticipant($currentTurnUser);
+    }
+
+    /**
+     * @param User $currentTurnUser
+     */
+    private function notifyNextParticipant(User $currentTurnUser)
+    {
+        $body = [
+            'type' => 'its_your_turn',
+            'data' => [
+                'user' => [
+                    'uuid' => $currentTurnUser->getUuid(),
+                ],
+            ],
+        ];
+
+        // notify next participant
+        $this->rabbit->publish('yarnyard', json_encode($body));
     }
 }
